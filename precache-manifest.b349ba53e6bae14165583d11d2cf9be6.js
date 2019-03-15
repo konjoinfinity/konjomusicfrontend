a1ab38cc27300cad555d6398db524598a50dea0e
Wesley Scholl <clients@konjoweb.com>
2019-03-15 11:53:11 -0400

    Updates

---
+++ precache-manifest.b349ba53e6bae14165583d11d2cf9be6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "275dd038ed396dc3b4c3",
+    "url": "/konjo-music/static/css/main.61636c72.chunk.css"
+  },
+  {
+    "revision": "275dd038ed396dc3b4c3",
+    "url": "/konjo-music/static/js/main.c3cab770.chunk.js"
+  },
+  {
+    "revision": "ba641d7d24badb6a75de",
+    "url": "/konjo-music/static/js/runtime~main.7a98b517.js"
+  },
+  {
+    "revision": "3fd37af8d822fe7762c3",
+    "url": "/konjo-music/static/js/2.ef5b6807.chunk.js"
+  },
+  {
+    "revision": "34d2c1abcb8435045e9de17bdd654a09",
+    "url": "/konjo-music/index.html"
+  }
+];
